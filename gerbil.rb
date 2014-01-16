7cae2c6e432d33d963320f162557d978fd7959cc
Eric Nelson <eric@clean-logix.com>
2014-01-16 14:30:28 -0500

    fixed loading

--- gerbil.rb
+++ gerbil.rb
@@ -3,4 +3,14 @@ $LOAD_PATH.unshift app unless $LOAD_PATH.include? app
 app = File.expand_path(File.join(File.dirname(__FILE__), "lib"))
 $LOAD_PATH.unshift app unless $LOAD_PATH.include? app
 
-require "consumers/garage_door_consumer"
+Dir.glob("app/consumers/*").each do |file|
+	require File.expand_path(file)
+end
+
+require 'yaml'
+
+config = YAML.load_file(File.join(File.dirname(__FILE__),"config", "hutch.yml"))
+config.each do |attr, value|
+  Hutch::Config.set attr.to_sym, value
+end
+
